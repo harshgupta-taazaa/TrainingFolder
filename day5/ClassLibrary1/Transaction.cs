c2d33b365eeecce8651ea452ff9616365df5cb3a
harshgupta-taazaa <harsh.gupta@taazaa.com>
2021-03-19 12:28:17 -0700

    first commit

---
+++ day5/ClassLibrary1/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Transaction
+    {
+
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Notes { get; set; }
+
+        public Transaction(decimal amount, DateTime data, string notes)
+        {
+            this.Amount = amount;
+            this.Date = Date;
+            this.Notes = notes;
+        }
+    }
+}
+
