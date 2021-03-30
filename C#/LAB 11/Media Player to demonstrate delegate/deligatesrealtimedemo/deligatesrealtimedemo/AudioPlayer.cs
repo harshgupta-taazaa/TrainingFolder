3bfb1fc49a5538dad4d79bfb51a68b02302f3354
harshgupta-taazaa <harsh.gupta@taazaa.com>
2021-03-29 23:53:43 -0700

    c#

---
+++ C#/LAB 11/Media Player to demonstrate delegate/deligatesrealtimedemo/deligatesrealtimedemo/AudioPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deligatesrealtimedemo
+{
+    public class AudioPlayer
+    {
+        private int VideoPlayerStatus;
+        public int PlayAudioFile(string a)
+        {
+            if (a == "mvw")
+                VideoPlayerStatus = 0 ;
+            else
+                VideoPlayerStatus = -1;
+            return VideoPlayerStatus;
+        }
+    }
+}
