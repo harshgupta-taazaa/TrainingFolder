b977e8d026033b4581da2c8d3a82c3e9a9a2f37f
harshgupta-taazaa <harsh.gupta@taazaa.com>
2021-04-20 13:13:53 -0700

    commit

--- productscatalog/src/app/product/Iproduct.ts
+++
@@ -1,8 +0,0 @@
-export interface IProduct{
-    ProductId:number,
-   ProductName:string,
-    Price:number,
-    CategoryName:string,
-    Manufacturer:string,
-    ShortCode:string
-}
--- productscatalog/src/app/productname-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ProductnamePipePipe } from './productname-pipe.pipe';
-
-describe('ProductnamePipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ProductnamePipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
