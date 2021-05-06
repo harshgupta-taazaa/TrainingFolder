9f5a9b471dfb0cd1224a91ab49e50e3f407be728
harshgupta-taazaa <harsh.gupta@taazaa.com>
2021-05-06 20:09:49 +0530

    commit

---
+++ ProductsApi/ProductsApi/Infrastructure/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsApi.Infrastructure
+{
+    public class ProductContext:DbContext
+    {
+        public DbSet<Product> ProductTable { get; set; }
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+    }
+}
